384a9b271ab38bfe24256e5d68b21cab23102731
Ricardo Santos <rsantos@blissapplications.com>
2021-01-20 19:44:04 +0000

    added CombineSimpleNetworkAgentRequestModel.init

--- Sources/RJSLibUFNetworking/Sources/CombineBasicNetworkClient/CombineSimpleNetworkAgentRequestModel.swift
+++ Sources/RJSLibUFNetworking/Sources/CombineBasicNetworkClient/CombineSimpleNetworkAgentRequestModel.swift
@@ -11,4 +11,16 @@ public struct CombineSimpleNetworkAgentRequestModel {
     public let httpBodyDic: [String: String]?
     public let headerValues: [String: String]?
     public let serverURL: String
+    
+    public init(path: String,
+                httpMethod: HttpMethod,
+                httpBodyDic: [String: String]?,
+                headerValues: [String: String]?,
+                serverURL: String) {
+        self.path = path
+        self.httpMethod = httpMethod
+        self.httpBodyDic = httpBodyDic
+        self.headerValues = headerValues
+        self.serverURL = serverURL
+    }
 }
